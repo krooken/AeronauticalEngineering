0c6775d7d7e2f713bb4ed0b7032497e23d7f3bd8
Jonas Krook <krook.jonas@gmail.com>
2018-12-25 13:34:58 +0100

    Adding functions for balloon equations. Adjusting values and names of constants.

--- StandardAtmosphere/StandardAtmosphere.py
+++ StandardAtmosphere/StandardAtmosphere.py
@@ -1,12 +1,14 @@
 # Constants
 
-R = 287.0
+R = 287.05
 K = 273.15
+g = 9.800
+
 temperature_at_sea_level = 15.0 + K
 pressure_at_sea_level = 101325
-air_density_at_sea_level = 1.225
+density_air_at_sea_level = 1.225
 
-molar_mass_helium = 4.0
+molar_mass_helium = 4.002
 molar_mass_nitrogen = 28.0
 molar_mass_air = 28.97
 
@@ -18,3 +20,38 @@ atmospheric_layers = [
 	('mesosphere', 47000.0, 51000.0, -2.8),
 	('mesosphere', 51000.0, 71000.0, -2.0)
 	]
+
+def pressure(
+	density=density_air_at_sea_level,
+	temperature=temperature_at_sea_level,
+	R = R):
+	return density*temperature*R
+	
+def density(
+	pressure=pressure_at_sea_level,
+	temperature=temperature_at_sea_level,
+	R = R):
+	return pressure/(temperature*R)
+	
+def balloon_lift_general(
+	volume, density_gas,
+	density_ambient=density_air_at_sea_level,
+	g=g):
+	return volume*g*density_ambient*(1-density_gas/density_ambient)
+	
+def balloon_lift_hot_air(
+	volume, temperature_difference,
+	temperature_ambient=temperature_at_sea_level,
+	density_ambient=density_air_at_sea_level,
+	g=g):
+	return volume*g*density_ambient \
+		* (temperature_difference/(temperature_difference+temperature_ambient))
+		
+def balloon_lift_gas(
+	volume, molar_mass_gas,
+	molar_mass_ambient=molar_mass_air,
+	density_ambient=density_air_at_sea_level,
+	g=g):
+	return volume*g*density_ambient \
+		* (1 - molar_mass_gas/molar_mass_ambient)
+		
